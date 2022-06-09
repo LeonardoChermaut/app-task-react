bccfe5605fe21d98e88436ba8017cdd15b055edf
Leonardo Chermaut <chermauts@hotmail.com>
2022-06-09 18:54:11 -0300

    refactor

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-
 import "./Button.css";
 
 const Button = ({ children, onClick }) => {
-	return (
-		<button onClick={onClick} className="button">
-			{children}
-		</button>
-	);
+  return (
+    <button onClick={onClick} className="button">
+      {children}
+    </button>
+  );
 };
 
 export default Button;
